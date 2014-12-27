0c40500b9ef51b480c9927d25a94e36f335ad245
Rhindi Youssef <wafa94@gmail.com>
2014-12-26 20:38:10 -0500

    lab1 fini

--- JSON-master/src/json/JSON.java
+++ JSON-master/src/json/JSON.java
@@ -1,4 +1,3 @@
-
 package json;
 
 import java.io.FileNotFoundException;
@@ -8,7 +7,17 @@ import net.sf.json.JSONObject;
 
 public class JSON {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    private static final String filePath = "C:\\Users\\RHINDI-LAP\\Desktop\\INF2015-40\\LABO\\lab1\\JSON-master\\src\\json\\jourSemaine.json";
 
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        JSONArray tableau = new JSONArray().fromObject(FileReader.loadFileIntoString(filePath, args[0]));
+
+        // affichage du fichier avec une boucle for normal
+//        for(int i = 0 ; i < tableau.size(); ++i){
+//            System.out.println(tableau.getString(i));
+//        }   
+        for (Object o : tableau) {
+            System.out.println(o);
+        }
     }
 }
